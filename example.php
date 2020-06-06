f089fea68ee2b5a7a205d10341125b5c04f81a78
Quiec <46392667+Quiec@users.noreply.github.com>
2020-06-06 04:49:55 +0300

    boting: More examples

--- example.php
+++ example.php
@@ -15,9 +15,13 @@ while (True) {
         $Bir = ["type" => "article", "id" => 0, "title" => "test", "input_message_content" => ["message_text" => "sad"]];
         $Bot->answerInlineQuery(["inline_query_id" => $Update["inline_query"]["id"], "results" => json_encode([$Bir])]);    
     } elseif (!empty($Update["message"])) {
-        if ($Update["message"]["text"] === "/start") echo $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "text" => "Started bot."]);    
+        if ($Update["message"]["text"] === "/start") $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "text" => "Started bot."]);    
         if ($Update["message"]["text"] === "/photo") $Bot->sendPhoto(["chat_id" => $Update["message"]["chat"]["id"], "photo" => "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Telegram_2019_Logo.svg/1200px-Telegram_2019_Logo.svg.png"]);    
         if ($Update["message"]["text"] === "/callback") $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "text" => "Example message for callback query", "reply_markup" => json_encode(["inline_keyboard" => [[["text" => "Click me", "callback_data" => "test"], ["text" => "Don't click me", "callback_data" => "test2"]]]]) ]);    
+        if ($Update["message"]["text"] === "/keyboard") $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "text" => "Example message for keyboard", "reply_markup" => json_encode(["keyboard" => [[["text" => "Click me"], ["text" => "Don't click me"]]]])]);    
+        if ($Update["message"]["text"] === "Click me") $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "text" => "Thanks for click"]);    
+        if ($Update["message"]["text"] === "Don't click me") $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "text" => "Why clicked?!", "reply_markup" => json_encode(["remove_keyboard" => TRUE])]);    
+    
     } elseif (!empty($Update["callback_query"])) {
         $Data = $Update["callback_query"]["data"];
         if ($Data == "test") {
