7bf5c401969170969cffcddc920936964de0b9c9
Quiec <46392667+Quiec@users.noreply.github.com>
2020-06-25 19:20:33 +0300

    boting: Version 1.1
    File download support, sent requests body.

--- src/Boting/Boting.php
+++ src/Boting/Boting.php
@@ -36,17 +36,37 @@ class Boting {
         }  
     }
 
+    public function downloadFile($FileId, $fileName = NULL) {
+        $Request = $this->Client->getAsync("getFile?file_id=$FileId")->wait();
+        $Body = $Request->getBody()->getContents();
+        $Result = json_decode($Body, true)["result"];
+        $Token = $this->Token;
+        $Download = new Client();
+
+        if ($fileName == NULL) {
+            preg_match_all('/\/(.*)\.(.*)/m', $Result["file_path"], $File, PREG_SET_ORDER, 0);
+            $fileName = $File[0][1] . "." . $File[0][2];
+        }
+    
+        $Request = $Download->getAsync("https://api.telegram.org" . "/file/bot" . $Token . "/" . $Result["file_path"])->wait()->getBody()->getContents();    
+    
+        file_put_contents($fileName, $Request);
+        return $fileName;
+    }
+
+
     public function __call($method, $args) {
-        $this->Request[] = $this->Client->postAsync($method, ["form_params" => $args[0]]);
-        Promise\unwrap($this->Request);
+        $this->Request = $this->Client->postAsync($method, ["form_params" => $args[0]])->wait();
         return $this;
     }
 
-    public function run() {
-        Promise\settle($this->Request)->wait();
+    public function body() {
+        $Req = $this->Request->getBody()->getContents();
+        return $Req;
     }
 
     public function Handler ($Token, $Function) {
+        $this->Token = $Token;
         $this->Client = new Client(["base_uri" => "https://api.telegram.org" . "/bot" . $Token . "/"]);
         while (True) {
             $Update = $this->getUpdates();
